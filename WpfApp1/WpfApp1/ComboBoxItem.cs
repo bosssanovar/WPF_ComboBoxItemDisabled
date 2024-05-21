ff109f65399e8289cc7f99368868be0762f93d49
kei ito <ito.kei@jvckenwood.com>
2024-05-22 08:16:06 +0900

    first commit

---
+++ WpfApp1/WpfApp1/ComboBoxItem.cs
@@ -0,0 +1,22 @@
+﻿using Reactive.Bindings;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class ComboBoxItem<T>
+    {
+        public required T Value { get; init; }
+
+        public required string DisplayText { get; init; }
+
+        public ReadOnlyReactivePropertySlim<bool> IsEnabled { get; init; } = new(Observable.Return(true), true);
+
+        public ReadOnlyReactivePropertySlim<bool> IsVisibled { get; init; } = new(Observable.Return(true), true);
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Reactive.Bindings;
+
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ReactivePropertySlim<bool> IsItemEnabled { get; } = new(true);
+
+        public ReactivePropertySlim<bool> IsItemVisibled { get; } = new(true);
+
+        public ReactiveCollection<ComboBoxItem<int>> Items { get; } = [];
+
+        public ReactivePropertySlim<int> SelectedValue { get; } = new(0);
+
+        public MainWindow()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 2)
+                {
+                    Items.Add(
+                        new()
+                        {
+                            Value = i,
+                            DisplayText = $"{i + 1} : item {i + 1} (有効無効切り替え)",
+                            IsEnabled = IsItemEnabled.ToReadOnlyReactivePropertySlim(true),
+                        });
+                }
+                else if (i == 3)
+                {
+                    Items.Add(
+                        new()
+                        {
+                            Value = i,
+                            DisplayText = $"{i + 1} : item {i + 1}（表示非表示切り替え）",
+                            IsVisibled = IsItemVisibled.ToReadOnlyReactivePropertySlim(true),
+                        });
+                }
+                else
+                {
+                    Items.Add(
+                        new()
+                        {
+                            Value = i,
+                            DisplayText = $"{i + 1} : item {i + 1}",
+                        });
+                }
+            }
+
+            InitializeComponent();
+        }
+    }
+}
